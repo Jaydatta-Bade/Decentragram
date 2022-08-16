df0b1687c09a58121936cd725da6fd42c20b86cb
Jaydatta Bade <jaydattabade11@gmail.com>
2022-08-16 17:38:57 +0530

    Updates

---
+++ precache-manifest.f5cd4e771a3abe69932d5eda63c0fd8c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ca6e20882539941e62de",
+    "url": "/decentragram/static/css/main.00c7979e.chunk.css"
+  },
+  {
+    "revision": "ca6e20882539941e62de",
+    "url": "/decentragram/static/js/main.ca6e2088.chunk.js"
+  },
+  {
+    "revision": "975f2dcb22cf01729787",
+    "url": "/decentragram/static/css/1.982530da.chunk.css"
+  },
+  {
+    "revision": "975f2dcb22cf01729787",
+    "url": "/decentragram/static/js/1.975f2dcb.chunk.js"
+  },
+  {
+    "revision": "a4a45a276db0bfa179c8",
+    "url": "/decentragram/static/js/runtime~main.a4a45a27.js"
+  },
+  {
+    "revision": "d6cbd8e6d17d679dd3cd7fe3884ac8bb",
+    "url": "/decentragram/static/media/photo.d6cbd8e6.png"
+  },
+  {
+    "revision": "393b05ba115e6cf58c5cf7ea81263a9d",
+    "url": "/decentragram/index.html"
+  }
+];
